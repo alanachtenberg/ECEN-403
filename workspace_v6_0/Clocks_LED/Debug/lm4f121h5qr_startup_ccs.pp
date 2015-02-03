493d58f38cef706cfb02a6b844fcd9b13350f95a
Alan <alanachtenberg@yahoo.com>
2015-02-03 13:40:37 -0600

    Finished project 2 and 3
    added projects 2 and 3

---
+++ workspace_v6_0/Clocks_LED/Debug/lm4f121h5qr_startup_ccs.pp
@@ -0,0 +1,7 @@
+# FIXED
+
+lm4f121h5qr_startup_ccs.obj: ../lm4f121h5qr_startup_ccs.c
+lm4f121h5qr_startup_ccs.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+
+../lm4f121h5qr_startup_ccs.c: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
