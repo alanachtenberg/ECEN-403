8e9caab92e1414193b3ad96a9ee72249c0ea64a6
Alan <alanachtenberg@yahoo.com>
2015-02-10 16:59:01 -0600

    mini-lab_2 project 1
    finished, need to run on the board later

---
+++ workspace/Project_1/Debug/main.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/StellarisWare/inc/hw_memmap.h
+main.obj: C:/StellarisWare/inc/hw_types.h
+main.obj: C:/StellarisWare/driverlib/debug.h
+main.obj: C:/StellarisWare/driverlib/sysctl.h
+main.obj: C:/StellarisWare/driverlib/adc.h
+
+../main.c: 
+C:/StellarisWare/inc/hw_memmap.h: 
+C:/StellarisWare/inc/hw_types.h: 
+C:/StellarisWare/driverlib/debug.h: 
+C:/StellarisWare/driverlib/sysctl.h: 
+C:/StellarisWare/driverlib/adc.h: 
