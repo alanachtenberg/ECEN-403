df54b74874084bb5a5c4f4510aeb1e57a8832e44
Alan <alanachtenberg@yahoo.com>
2015-04-28 16:05:46 -0500

    pushing and pulling changes

--- AndroidApp/app/src/main/java/alan/cardiac/TcpCommThread.java
+++ AndroidApp/app/src/main/java/alan/cardiac/TcpCommThread.java
@@ -1,5 +1,8 @@
 package alan.cardiac;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,29 +15,91 @@ import java.net.Socket;
  * Created by Alan on 4/15/2015.
  */
 public class TcpCommThread implements Runnable{
-    Socket socket;
-    InetAddress hostAddress;
-    BufferedReader input;
-    PrintWriter output;
-    String serverPass;
+    private Context context;
+
+    private Socket socket;
+    private InetAddress hostAddress;
+    private BufferedReader input;
+    private PrintWriter output;
+    private String serverPass;
+    private String userName;
     private int port;
-    TcpCommThread(String host,String portNum, String pass) throws IOException {
+
+
+
+    TcpCommThread(String host,String portNum, String user, String pass, Context c) throws IOException {
+        c=context;
         hostAddress= Inet4Address.getByName(host);
+        userName=user;
         serverPass=pass;
         port=Integer.parseInt(portNum);
+
     }
 
     public void run(){
+        if  (!connect()) {//try to connect
+            Toast.makeText(context, "Failed to connect to server", Toast.LENGTH_SHORT);
+        }
+        else if (!authenticate()) {//try to authenticate
+            Toast.makeText(context,"Failed to authenticate with Server, incorrect password",Toast.LENGTH_SHORT);
+            close();//close the connection
+        }
+        else{//proceed to logic
+            //TODO cool stuff man
+            writeLn("Hello i am an android mobile device");
+
+            //close();
+        }
+
+    }
+
+    private Boolean authenticate(){
+        writeLn(serverPass);
+        String message=readLn();
+        if (message.toLowerCase().startsWith("incorrect")){
+            return false;
+        }
+        writeLn(userName);
+        return true;
+    }
+    public void writeLn(String s){
+        output.println(s);
+        output.flush();
+    }
+    public String readLn(){
         try {
-            socket= new Socket(hostAddress,port);
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new PrintWriter(socket.getOutputStream());
-            while (true) {
-                String message = input.readLine();
-            }
+            String m=null;
+            m = input.readLine();
+            return m;
         }
         catch (IOException e){
+            e.printStackTrace();
+            close();
+        }
+        return null;
+    }
 
+    private Boolean connect(){
+        try {
+            socket = new Socket(hostAddress, port);
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream());
+            return true;
+        }
+        catch (IOException e){
+            close();
+        }
+        return false;
+    }
+
+    public void close(){
+        if (socket!=null&& socket.isConnected()){
+            try {
+                TcpService.connected=false;//let service know
+                socket.close();
+            } catch (IOException e) {
+                socket=null;
+            }
         }
     }
 }
--- AndroidApp/app/src/main/java/alan/cardiac/TcpService.java
+++ AndroidApp/app/src/main/java/alan/cardiac/TcpService.java
@@ -1,8 +1,11 @@
 package alan.cardiac;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
@@ -17,10 +20,10 @@ public class TcpService extends Service {
     private SharedPreferences mSP;
 
     private Socket clientSocket;
-    private Boolean connected;
-    private String host;
-    private String port;
-    private String pass;
+    public static Boolean connected;
+    private String host, port;
+    private String user, pass;
+
     Thread tcpCommThread;
 
     @Override
@@ -38,27 +41,35 @@ public class TcpService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        host=(String)mSP.getString("host_address","localhost");
-        port=(String)mSP.getString("port_num","1234");
-        pass=(String)mSP.getString("server_pass","CARS");
-        if (!connected){
-            try {
-                //TODO fix exception error
-                connected=true;
-                tcpCommThread = new Thread(new TcpCommThread(host,port,pass));
-                tcpCommThread.start();
-            } catch (IOException e) {
-                connected=false;
-                e.printStackTrace();
+        try {
+            host=(String)mSP.getString("host_address","localhost");
+            port=(String)mSP.getString("port_num","1234");
+            pass=(String)mSP.getString("server_pass","CARS");
+            user=(String)mSP.getString("user_name","Android");
+            if (!connected) {
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    connected = true;
+                    tcpCommThread = new Thread(new TcpCommThread(host, port, user, pass,getApplicationContext()));
+                    tcpCommThread.start();
+                } else {
+                    //TODO make sure network connection is available
+                }
+
             }
-
+            /*START_NOT_STICKY Tells os to not recreate service if it is stopped in the event to free memmory.
+            * In other words, we will let the user trigger starting a new service in the event
+            * that it is stopped
+             */
+            return START_NOT_STICKY;
+        } catch (IOException e) {
+            connected=false;
+            e.printStackTrace();
+            stopSelf();//stop the TCP service, let user have a chance to update settings
         }
-
-        /*START_NOT_STICKY Tells os to not recreate service if it is stopped in the event to free memmory.
-        * In other words, we will let the user trigger starting a new service in the event
-        * that it is stopped
-         */
-        return START_NOT_STICKY;
+            return START_NOT_STICKY;
     }
 
     @Override
