b097d254e1215a46bc1ed8e32abd92c60a689965
Dennis Moore <denmoore93@gmail.com>
2015-09-12 01:37:27 -0500

    Read 4 floats from arduino

--- Serial/serial_reader.py
+++ Serial/serial_reader.py
@@ -14,30 +14,23 @@ def main():
     # Memory map the file
     buf = mmap.mmap(fd, mmap.PAGESIZE)
   
-    i = None
-    ##s = None
-  
     while 1:
-        #new_i, = struct.unpack('f', buf[:4])
-        #new_s, = struct.unpack('f', buf[4:8])
-  
+
         ser0, = unloadSerialData(buf, UdooSharedMem['ser0'])
-	ser1, = unloadSerialData(buf, UdooSharedMem['ser1'])
-	ser2, = unloadSerialData(buf, UdooSharedMem['ser2'])
-	ser3, = unloadSerialData(buf, UdooSharedMem['ser3'])
-	ser4, = unloadSerialData(buf, UdooSharedMem['ser4'])
+        ser1, = unloadSerialData(buf, UdooSharedMem['ser1'])
+        ser2, = unloadSerialData(buf, UdooSharedMem['ser2'])
+        ser3, = unloadSerialData(buf, UdooSharedMem['ser3'])
+        #ser4, = unloadSerialData(buf, UdooSharedMem['ser4'])
  
-        if i != ser0:
-            print 'i: %s => %f' % (i, ser0)
-	    print 'ser1: %s' % (ser1)
-	    print 'ser2: %s' % (ser2)
-	    print 'ser3: %s' % (ser3)
-	    print 'ser4: %s' % (ser4)
-            print 'Press Ctrl-C to exit'
-            i = ser0
-  
-        time.sleep(1) 
-  ## Delete fd when done
+
+        print 'ser0: %s' % (ser0)
+        print 'ser1: %s' % (ser1)
+        print 'ser2: %s' % (ser2)
+        print 'ser3: %s' % (ser3)
+        #print 'ser4: %s' % (ser4)
+        print 'Press Ctrl-C to exit'
+
+  ## Delete fd when done?
   
 if __name__ == '__main__':
     main()
