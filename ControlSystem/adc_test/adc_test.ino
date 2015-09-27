78a09a477b3969821da4547e7bf4eac1a3ad0bc0
Dennis <denmoore93@gmail.com>
2015-09-27 13:05:19 -0500

    adc_test code compiles. needs to be tested

--- ControlSystem/adc_test/adc_test.ino
+++ ControlSystem/adc_test/adc_test.ino
@@ -1,57 +1,60 @@
-// Circular buffer, power of two.
-#define BUFSIZE 0x400
-#define BUFMASK 0x3FF
-volatile int samples [BUFSIZE] ;
-volatile int sptr = 0 ;
-volatile int isr_count = 0 ;   // this was for debugging
-
-
 void setup()
 {
-  Serial.begin (115200) ; // for debugging
+  Serial.begin (57600) ; // was for debugging
   adc_setup () ;         // setup ADC
+  
+  pmc_enable_periph_clk (TC_INTERFACE_ID + 0*3+0) ;  // clock the TC0 channel 0
+
+  TcChannel * t = &(TC0->TC_CHANNEL)[0] ;    // pointer to TC0 registers for its channel 0
+  t->TC_CCR = TC_CCR_CLKDIS ;  // disable internal clocking while setup regs
+  t->TC_IDR = 0xFFFFFFFF ;     // disable interrupts
+  t->TC_SR ;                   // read int status reg to clear pending
+  t->TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK1 |   // use TCLK1 (prescale by 2, = 42MHz)
+              TC_CMR_WAVE |                  // waveform mode
+              TC_CMR_WAVSEL_UP_RC |          // count-up PWM using RC as threshold
+              TC_CMR_EEVT_XC0 |     // Set external events from XC0 (this setup TIOB as output)
+              TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_CLEAR |
+              TC_CMR_BCPB_CLEAR | TC_CMR_BCPC_CLEAR ;
+  
+  t->TC_RC =  875 ;     // counter resets on RC, so sets period in terms of 42MHz clock
+  t->TC_RA =  440 ;     // roughly square wave
+  t->TC_CMR = (t->TC_CMR & 0xFFF0FFFF) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_SET ;  // set clear and set from RA and RC compares
+  
+  t->TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG ;  // re-enable local clocking and switch to hardware trigger source.
+
   setup_pio_TIOA0 () ;  // drive Arduino pin 2 at 48kHz to bring clock out
-  // Start timer. Parameters are:
-
-  // TC0 : timer counter. Can be TC0, TC1 or TC2
-  // 0   : channel. Can be 0, 1 or 2
-  // TC0_IRQn: irq number. See table.
-  // 1  : frequency (in Hz)
-  // The interrupt service routine is TC0_Handler. See table.
-
-  startTimer(TC0, 0, TC0_IRQn, 1);
-
-  // Paramters table:
-  // TC0, 0, TC0_IRQn  =>  TC0_Handler()
-  // TC0, 1, TC1_IRQn  =>  TC1_Handler()
-  // TC0, 2, TC2_IRQn  =>  TC2_Handler()
-  // TC1, 0, TC3_IRQn  =>  TC3_Handler()
-  // TC1, 1, TC4_IRQn  =>  TC4_Handler()
-  // TC1, 2, TC5_IRQn  =>  TC5_Handler()
-  // TC2, 0, TC6_IRQn  =>  TC6_Handler()
-  // TC2, 1, TC7_IRQn  =>  TC7_Handler()
-  // TC2, 2, TC8_IRQn  =>  TC8_Handler()
+  dac_setup () ;        // setup up DAC auto-triggered at 48kHz
 }
 
-void loop() 
+void setup_pio_TIOA0 ()  // Configure Ard pin 2 as output from TC0 channel A (copy of trigger event)
 {
+  PIOB->PIO_PDR = PIO_PB25B_TIOA0 ;  // disable PIO control
+  PIOB->PIO_IDR = PIO_PB25B_TIOA0 ;   // disable PIO interrupts
+  PIOB->PIO_ABSR |= PIO_PB25B_TIOA0 ;  // switch to B peripheral
 }
 
 
-/* Function Defines */
+void dac_setup ()
+{
+  pmc_enable_periph_clk (DACC_INTERFACE_ID) ; // start clocking DAC
+  DACC->DACC_CR = DACC_CR_SWRST ;  // reset DAC
 
-void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
-  pmc_set_writeprotect(false);
-  pmc_enable_periph_clk((uint32_t)irq);
-  TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
-  uint32_t rc = VARIANT_MCK/128/frequency; //128 because we selected TIMER_CLOCK4 above
-  TC_SetRA(tc, channel, rc/2); //50% high, 50% low
-  TC_SetRC(tc, channel, rc);
-  TC_Start(tc, channel);
-  tc->TC_CHANNEL[channel].TC_IER=TC_IER_CPCS;
-  tc->TC_CHANNEL[channel].TC_IDR=~TC_IER_CPCS;
-  NVIC_EnableIRQ(irq);
+  DACC->DACC_MR = 
+    DACC_MR_TRGEN_EN | DACC_MR_TRGSEL (1) |  // trigger 1 = TIO output of TC0
+    (0 << DACC_MR_USER_SEL_Pos) |  // select channel 0
+    DACC_MR_REFRESH (0x0F) |       // bit of a guess... I'm assuming refresh not needed at 48kHz
+    (24 << DACC_MR_STARTUP_Pos) ;  // 24 = 1536 cycles which I think is in range 23..45us since DAC clock = 42MHz
+
+  DACC->DACC_IDR = 0xFFFFFFFF ; // no interrupts
+  DACC->DACC_CHER = DACC_CHER_CH0 << 0 ; // enable chan0
 }
+
+void dac_write (int val)
+{
+  DACC->DACC_CDR = val & 0xFFF ;
+}
+
+
 
 void adc_setup ()
 {
@@ -66,26 +69,13 @@ void adc_setup ()
   ADC->ADC_MR = (ADC->ADC_MR & 0xFFFFFFF0) | (1 << 1) | ADC_MR_TRGEN ;  // 1 = trig source TIO from TC0
 }
 
-void setup_pio_TIOA0 ()  // Configure Ard pin 2 as output from TC0 channel A (copy of trigger event)
-{
-  PIOB->PIO_PDR = PIO_PB25B_TIOA0 ;  // disable PIO control
-  PIOB->PIO_IDR = PIO_PB25B_TIOA0 ;   // disable PIO interrupts
-  PIOB->PIO_ABSR |= PIO_PB25B_TIOA0 ;  // switch to B peripheral
-}
+// Circular buffer, power of two.
+#define BUFSIZE 0x400
+#define BUFMASK 0x3FF
+volatile int samples [BUFSIZE] ;
+volatile int sptr = 0 ;
+volatile int isr_count = 0 ;   // this was for debugging
 
-
-/* Interrupt Handlers */
-
-// This function is called every 1 sec.
-void TC0_Handler()
-{
-  // You must do TC_GetStatus to "accept" interrupt
-  // As parameters use the first two parameters used in startTimer (TC0, 0 in this case)
-  TC_GetStatus(TC0, 0);
-  Serial.print("time(ms): "):
-  Serial.print(millis());
-
-}
 
 #ifdef __cplusplus
 extern "C" 
@@ -97,10 +87,9 @@ void ADC_Handler (void)
   if (ADC->ADC_ISR & ADC_ISR_EOC7)   // ensure there was an End-of-Conversion and we read the ISR reg
   {
     int val = *(ADC->ADC_CDR+7) ;    // get conversion result
-    Serial.print("ADC Value: ");
-    Serial.println(val);
     samples [sptr] = val ;           // stick in circular buffer
     sptr = (sptr+1) & BUFMASK ;      // move pointer
+    dac_write (0xFFF & ~val) ;       // copy inverted to DAC output FIFO
   }
   isr_count ++ ;
 }
@@ -108,3 +97,8 @@ void ADC_Handler (void)
 #ifdef __cplusplus
 }
 #endif
+
+
+void loop() 
+{
+}
