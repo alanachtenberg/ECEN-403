9921990f37d815620ec68bd780c1631cc403742e
Dennis <denmoore93@gmail.com>
2015-10-10 16:49:17 -0500

    first version of peak maximum detection

--- ControlSystem/ecg/ecg.ino
+++ ControlSystem/ecg/ecg.ino
@@ -2,4 +2,28 @@
 // The freqency of the timer interrupt can be changed, 
 // and thus the sample frequency can be changed.
 
+int curr_max = 0;
+
+//Structure to hold ADC values and their times
+
+struct Heart
+{
+  int value; // voltage value from ADC
+  int tyme; // time of measurement
+};
+
+Heart peak[1000]; //Might want to make this a dynamic array
+
+// peak.value[i] = adc measurement
+// peak.tyme[i] = micros()
+
+
+// search for the max value
+for (int i = 0; i < 1000; i++)
+{
+  if (peak.value[i] > curr_max)
+  {
+    curr_max = peak.value[i];
+  }
+}
 
