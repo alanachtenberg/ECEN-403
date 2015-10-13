3b5873df6ac5c9f075a4803b0b2603901ef7895a
Dennis <denmoore93@gmail.com>
2015-10-13 01:32:43 -0500

    buffer size changed to 675

--- ControlSystem/ecg/ecg.ino
+++ ControlSystem/ecg/ecg.ino
@@ -2,6 +2,30 @@
 // The freqency of the timer interrupt can be changed, 
 // and thus the sample frequency can be changed.
 
+
+//////////////////////////
+/*  Inside ADC Handler  */
+//////////////////////////
+void adc_handler()
+{
+  peak.value[i] = adc measurement
+  peak.tyme[i] = micros()
+  if (i == 675) // wait until you get 675 samples
+  {
+    i = 0;
+    // set flag maybe
+    // search through array for peaks
+    for (int j = 0; j < 200; j++)
+    {
+      if (peak.value[j] > curr_max)
+      {
+        curr_max = peak.value[i];
+      }
+    }
+  }
+  i++;
+}
+
 int curr_max = 0;
 
 //Structure to hold ADC values and their times
@@ -13,17 +37,4 @@ struct Heart
 };
 
 Heart peak[200]; //Might want to make this a dynamic array
-
-// peak.value[i] = adc measurement
-// peak.tyme[i] = micros()
-
-
-// search for the max value
-for (int i = 0; i < 200; i++)
-{
-  if (peak.value[i] > curr_max)
-  {
-    curr_max = peak.value[i];
-  }
-}
 
