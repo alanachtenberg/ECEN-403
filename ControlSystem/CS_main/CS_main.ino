685731bee6851eaedf4e2a1e70e8b2393d4a2080
kdillongto <kdillongto@tamu.edu>
2015-10-20 19:21:58 -0500

    move code into csmain

--- ControlSystem/CS_main/CS_main.ino
+++ ControlSystem/CS_main/CS_main.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+
 
 // These are the clock frequencies available to the timers /2,/8,/32,/128
 // 84Mhz/2 = 42.000 MHz
@@ -16,25 +18,38 @@
 #define    MeasureValue        0x04          // Value to initiate ranging.
 #define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
 #define    BUFF_SIZE 7 //size of buffer for time and distance measurements
-#define    circum 131.94//cm
+#define circum 1.2767// meters
 #define    rxn_time 4.8//seconds
-#define    decel_max 223.5//cm/s^2 (assumption)
+#define    decel_max 2.235//cm/s^2 (assumption)
 long reading = 0;
+float distance = 0;
 volatile unsigned int quarter_revolutions;
-float rps;
-float v1 =0;
+float rpm;
+float v1 = 0;
+float v2 = 0;
+float v3 = 0;
 unsigned long timeold;//in milliseconds
-float current_time=0; //in milliseconds
-float time_buff[7];//in sedconds
-volatile float lidar_distance[7];//cmeters
+unsigned long current_time=0; //in milliseconds
+float time1 = 0;
+float time_btwn = 0;
+float revolutions = 0;
+unsigned long time_buff[BUFF_SIZE];//in sedconds
+float distance_buff[BUFF_SIZE];//cmeters
 unsigned int   values_read=0;//current amount of values read from lidar
 unsigned int   old_values_read=0;//amount of values read since main loop last executed
 float rel_vel = 0;// cm/s
+float TTC = 0;
+
 float dummy_vel = 0;
-float t_stop = 0;//s
+float t_stop = 1;//s
 float v_stop_avg = 0;
 float d_stop = 0;
 float t_collision = 0;
+float rel_accel = 0;
+float rel_vel_time = 0;
+float rel_vel_buff[BUFF_SIZE-2];
+float accel_time_buff[BUFF_SIZE-2];
+
 
 
 
@@ -44,10 +59,10 @@ void setup()
   Serial.begin(115200); // start serial communication at 9600bps
   pinMode(12, INPUT);
   pinMode(13, INPUT);
-  attachInterrupt(12, magnet_detect, RISING);//Initialize the north pole detection intterrupt(o/p high) pin (Arduino digital pin 12)
-  attachInterrupt(13, magnet_detect2, FALLING);//south pole detection interrupt(o/p low)
+  attachInterrupt(13, magnet_detect, RISING);//Initialize the intterrupt pin (Arduino digital pin 2)
+
   quarter_revolutions = 0;
-  rps = 0;
+  rpm = 0;
   timeold = 0;
   
   /* turn on the timer clock in the power management controller */
@@ -57,7 +72,7 @@ void setup()
 
   /* we want wavesel 01 with RC */
   TC_Configure(/* clock */TC2,/* channel */1, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4); 
-  TC_SetRC(TC2, 1, 65600);//run every .1 seconds. 131200 for 5*/sec
+  TC_SetRC(TC2, 1, 65600);//run interrupt every .1 sec. f_lidar = 50hz ==> 5hz sampling rate
   TC_Start(TC2, 1);
 
   // enable timer interrupts on the timer
@@ -73,58 +88,52 @@ void setup()
   void loop()
   {
     //queue initially filled with zeroes; wait on queue to be filled before calculation
-    if (values_read > BUFF_SIZE && values_read!=old_values_read)
-    {
-      dummy_vel=0;
-      rel_vel=0;//init to 0 every time so that it does not continually increase
-      for (int i=0;i<BUFF_SIZE;++i)
-      {
-        float delta_d=(lidar_distance[i+1]-lidar_distance[i]);
-        float delta_t=(time_buff[i+1]-time_buff[i]);
-        float delta_v=(delta_d/delta_t);
-        rel_vel+=delta_v;
-        //dummy_vel+=delta_v;//for collision function
-      }
     
-      rel_vel=rel_vel/(BUFF_SIZE-1);//minus 1, we lose a value from the difference of distance and time
-      //dummy_vel = dummy_vel/(BUFF_SIZE-1);
-      //t_collision = lidar_distance[BUFF_SIZE-1]/dummy_vel;
-      
-      
-      Serial.print("Relative Velocity(cm/s) : ");
+    //collision(distance, rel_vel, rel_accel);
+         if (values_read > BUFF_SIZE && values_read!=old_values_read)
+    {
+     
+      rel_vel=0;//init to 0 every time so that it does not continually increase
+      rel_vel = relative_velocity();
+  
+      Serial.print("Relative Velocity(m/s) : ");
       Serial.println(rel_vel);
       Serial.print("\n");
-      //Serial.print("dummyV(cm/s) : ");
-      //Serial.println(dummy_vel);
-      //Serial.print("\n");
-    
-       //Serial.print("colTime: ");
-      //Serial.println(t_collision);
       old_values_read=values_read;
-      
-      //Serial.print("dist: ");
-      //Serial.println(reading);
-     
-      if (quarter_revolutions >=4) 
-      { //calculate revolution per second using 4 magnets
-        rps = quarter_revolutions*250/(float)((millis() - timeold));
-        v1 = rps*circum;//vehicle speed
-        timeold = millis();
-        quarter_revolutions = 0;
-        v_stop_avg = v1/2;
-        t_stop = (-v1)/(-decel_max);//time to stop based on estimated max decel
-        d_stop = v_stop_avg*t_stop;//distance to stop
-        //collision ();//Call function to detect if collision may occur
-        Serial.print("Velocity(cm/s): ");
-        Serial.println(v1);
-        Serial.print("\n");
-        Serial.print("stopping distance: ");
-        Serial.println(d_stop);
-        Serial.print("\n");
-      }
+    
+      push_velocity(rel_vel);
+
+      rel_accel=0;
+      rel_accel = relative_acceleration();
+      Serial.print("Relative Acceleration(m/s^2) : ");
+      Serial.println(rel_accel);
+      Serial.print("\n");
+    user_speed();
+
+       TTC = -rel_vel-(sqrt(sq(rel_vel)-2*rel_accel*(distance/100))/rel_accel);
+       Serial.println(TTC);
+    
+    }
+//      if (quarter_revolutions >=4) 
+//      { //calculate revolution per second using 4 magnets
+//        rps = quarter_revolutions*250/(float)((millis() - timeold));
+//        v1 = rps*circum;//vehicle speed
+//        timeold = millis();
+//        quarter_revolutions = 0;
+//        v_stop_avg = v1/2;
+//        t_stop = (-v1)/(-decel_max);//time to stop based on estimated max decel
+//        d_stop = v_stop_avg*t_stop;//distance to stop
+//        //collision ();//Call function to detect if collision may occur
+//        Serial.print("Velocity(cm/s): ");
+//        Serial.println(v1);
+//        Serial.print("\n");
+//        Serial.print("stopping distance: ");
+//        Serial.println(d_stop);
+//        Serial.print("\n");
+//      }
       
   
-    }
+    
     
   }
 
@@ -155,55 +164,134 @@ void setup()
     reading = Wire.read(); // receive high byte (overwrites previous reading)
     reading = reading << 8; // shift high byte to be high 8 bits
     reading |= Wire.read(); // receive low byte as lower 8 bits
-    current_time = millis();
+    distance = (float)reading; //convert to meters /100
+
+    current_time = millis(); //convert to seconds /1000
+   
+   
     
-    if(reading < 3500 && reading > 7){//set max (35 meters) and min (.05 meters) distance values to disregard distance errors
+    
+    if(distance < 3500 && distance > 7){//set max (35 meters) and min (.05 meters) distance values to disregard distance errors
       //reset rel_vel calculation if unrealistic change in distance occurs
-      if((lidar_distance[BUFF_SIZE-1]-reading) > 100 || (lidar_distance[BUFF_SIZE-1]-reading) < -100){
-        values_read = 0;
-      }
-      for (int i=0;i<BUFF_SIZE-1;++i){//shift all values in queues left
-        time_buff[i]=time_buff[i+1];
-        lidar_distance[i]=lidar_distance[i+1];
-      }  
-      //assign new values to end of queue
-      time_buff[BUFF_SIZE-1] = (float)current_time/1000;
-      lidar_distance[BUFF_SIZE-1] = (float)reading;
-    
-      Serial.print("Distance(cm): ");
-      Serial.println(reading); // print distance
+//      if((distance_buff[BUFF_SIZE-1]-distance) > 50 || (distance_buff[BUFF_SIZE-1]-distance) < -50){
+//        values_read = 0;
+//      }
       
+      push_distance_time(distance, current_time);
+    
+      Serial.print("Distance(m): ");
+      Serial.println(distance/100); // print distance
       ++values_read;//increment values read
     }    // We need to get the status to clear it and allow the interrupt to fire again
+    
+
  }
 
- void magnet_detect()//This function is called whenever a north pole magnet/interrupt is detected by the hall sensor
+ void magnet_detect()//This function is called whenever a magnet/interrupt is detected by the arduino
  {
    quarter_revolutions++;
-   //Serial.println("detect");
-   //Serial.println(rps);
+   //Serial.println(quarter_revolutions);
+   //Serial.println(rpm);
  }
- void magnet_detect2()//called when south pole detected by hall sensor. hall o/p set to low
+ 
+ void user_speed(){
+    if (quarter_revolutions >=4) { 
+     rpm = 60*quarter_revolutions*250/(float)((millis() - timeold));
+     v1 = rpm*circum; //cm/s
+     v2 = v1*.0223694*2; //mph
+     v3 = v1*2/100; // m/s
+     
+     timeold = millis();
+     quarter_revolutions = 0;
+     revolutions += 1; 
+     Serial.println("User Speed (mph): ");
+     Serial.println(v2);
+     
+     
+ }
+ }
+ 
+ float relative_velocity()
  {
-   quarter_revolutions++;
-   //Serial.println("detect2");
-   //Serial.println(rps);
- }
+   float delta_d=0;
+   float delta_t=0;
+   float rel_velo=0;
+   float rel_vel_sum = 0;
 
-   /*void collision(){
-   t_collision = lidar_distance[BUFF_SIZE-1]/dummy_vel;
-   if(t_stop < (t_collision+10)){
-     int j = 0;//do nothing
-   }
-   else if(t_stop < (t_collision+5)) {
-     Serial.print("t_stop: ");
-     Serial.println(t_stop);
-     Serial.print("t_collide: ");
-     Serial.println(t_collision);
-     Serial.println("BREAK!!! ");
-     Serial.print(" ");
-   }
-   }*/
+
+   for (int i=0;i<BUFF_SIZE-1;++i)
+      {
+        delta_d=(distance_buff[i+1]-distance_buff[i]);
+        delta_t=(time_buff[i+1]-time_buff[i]);
+        rel_velo=(delta_d/delta_t);
+        rel_vel_sum+=rel_velo;
+        
+      }
+    
+    return rel_vel_sum/(BUFF_SIZE-1);
+
+ }
+ 
+  float relative_acceleration()
+ {
+   float rel_a=0;
+   float rel_a_sum = 0;
+   float delta_rel_vel=0;
+   float delta_t=0;
+   for (int i=0;i<BUFF_SIZE-2;++i)
+      {
+        delta_rel_vel=(rel_vel_buff[i+1]-rel_vel_buff[i]);
+        delta_t=(time_buff[i+2]-time_buff[i+1]);
+        rel_a=(delta_rel_vel/delta_t);
+        rel_a_sum+=rel_a;
+        
+      }
+    
+    return rel_a_sum/(BUFF_SIZE-2);
+
+ }
+ 
+ void push_distance_time(float distance, float time )
+ {
+   for (int i=0;i<BUFF_SIZE-1;++i){//shift all values in queues left
+        distance_buff[i]=distance_buff[i+1];
+        time_buff[i]=time_buff[i+1];
+      }  
+      //assign new values to end of queue
+      distance_buff[BUFF_SIZE-1] = distance/100;
+      time_buff[BUFF_SIZE-1] = time/1000;
+  
+ }
+ 
+  void push_velocity(float velocity)
+ {
+   for (int i=0;i<BUFF_SIZE-2;++i){//shift all values in queues left
+        rel_vel_buff[i]=rel_vel_buff[i+1];
+      }  
+      //assign new values to end of que
+      rel_vel_buff[BUFF_SIZE-2] = velocity;// convert to seconds
+  
+ }
+ 
+ 
+ 
+
+ //void collision(float dist, float relVel, float relAccel){
+   //TTC = -relVel-sqrt(sq(relVel)-2*relAccel*dist)/relAccel;
+   //Serial.println(TTC);
+
+//   if(t_stop < (t_collision+10)){
+//     int j = 0;//do nothing
+//   }
+//   else if(t_stop < (t_collision+5)) {
+//     Serial.print("t_stop: ");
+//     Serial.println(t_stop);
+//     Serial.print("t_collide: ");
+//     Serial.println(t_collision);
+//     Serial.println("BREAK!!! ");
+//     Serial.print(" ");
+//   }
+//}
      
      
    
