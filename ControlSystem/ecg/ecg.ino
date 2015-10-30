186ffc8cb32872fbf50d17761522903da72bc9f5
Dennis <denmoore93@gmail.com>
2015-10-29 22:48:25 -0500

    detecting peaks at multiple signal frequencies. some times weird errors occur

--- ControlSystem/ecg/ecg.ino
+++ ControlSystem/ecg/ecg.ino
@@ -19,12 +19,13 @@ Peaks peak_max_array[10];
 
 Heart peak[100]; //Might want to make this a dynamic array
 
-int curr_max = 0;
+int curr_max = 1500;
 int i = 0;
-int threshold = 1000;
+int threshold = 1800;
 int peak_index = 0;
 int zero_index = 0;
 int max_index = 0;
+int adc_flag = 0;
 
 void setup()
 {
@@ -45,8 +46,8 @@ void setup()
               TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_CLEAR |
               TC_CMR_BCPB_CLEAR | TC_CMR_BCPC_CLEAR ;
   
-  t->TC_RC =  420000 ;     // counter resets on RC, so sets period in terms of 42MHz clock
-  t->TC_RA =  210000 ;     // roughly square wave
+  t->TC_RC =  840000 ;     // 50 Hz; counter resets on RC, so sets period in terms of 42MHz clock
+  t->TC_RA =  420000 ;     // roughly square wave
   t->TC_CMR = (t->TC_CMR & 0xFFF0FFFF) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_SET ;  // set clear and set from RA and RC compares
   
   t->TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG ;  // re-enable local clocking and switch to hardware trigger source.
@@ -89,40 +90,9 @@ void ADC_Handler (void)
     //Serial.println(micros());
     peak[i].value = val;
     peak[i].tyme = micros();
+    
+    adc_flag = 1;
 
-    if (peak[i].value > curr_max)
-    {
-      curr_max = peak[i].value;
-      max_index = i;
-    }
-    else if (peak[i].value == 0) // this value needs to be adjusted to what the adc reads at 0 V
-    {
-      zero_index = i;
-    }
-    else
-    {
-      if (zero_index > max_index && curr_max >= threshold) // some minimal adc value that peaks are never below
-      {
-        peak_max_array[peak_index].Value = curr_max; // need to be global variables. increment in main loop
-        //peak_max_array[peak_index].Tyme = millis();
-        //Serial.print("PV: ");
-        Serial.println(curr_max);
-        peak_index++;
-        curr_max = 0; // reset to 0 to detect new peaks
-      }  
-    }
-    
-    i++;
-    if (i == 101) //675 + 1 samples
-    {
-      i = 0;
-      // set flag and process data
-    }
-    
-    if (peak_index == 11)
-    {
-      peak_index = 0;
-    }
   
   }
 
@@ -135,7 +105,49 @@ void ADC_Handler (void)
 
 void loop()
 {
-
+  if (adc_flag == 1)
+  {
+    if (peak[i].value > curr_max)
+    {
+      curr_max = peak[i].value;
+      max_index = i;
+    }
+    
+    else if (peak[i].value <= 1250 && peak[i].value > 1000) // this value needs to be adjusted to what the adc reads at 0 V
+    {
+      zero_index = i;
+    }
+    
+    else
+    {
+      if (zero_index > max_index && curr_max >= threshold) // some minimal adc value that peaks are never below
+      {
+        peak_max_array[peak_index].Value = curr_max; // need to be global variables. increment in main loop
+        //peak_max_array[peak_index].Tyme = millis();
+        Serial.print("PV: ");
+        Serial.println(peak_max_array[peak_index].Value);
+        peak_index++;
+        curr_max = 1500; // reset to 0 to detect new peaks
+      }  
+    }
+    
+    i++;
+    
+    if (i == 100) //675 + 1 samples
+    {
+      i = 0;
+      // set flag and process data
+    }
+    
+    if (peak_index == 10)
+    {
+      peak_index = 0;
+    }
+    
+  adc_flag = 0;
+  
+  }
+  
 }
 
 
