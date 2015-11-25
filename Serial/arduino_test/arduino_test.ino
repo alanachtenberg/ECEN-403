a3eba7cd1f85e9babf5c8f5a96982c94ccb001a1
Dennis <denmoore93@gmail.com>
2015-11-25 17:47:18 -0600

    changed arduino test code to reflect changed shared mem

--- Serial/arduino_test/arduino_test.ino
+++ Serial/arduino_test/arduino_test.ino
@@ -1,3 +1,14 @@
+char EcgHdr = 'e';
+float BPM = 60;
+int MbFlag = 1;
+int LvpFlag = 1;
+float LvpValue = 2;
+char EcgFtr = 'f';
+char KineHdr = 'k';
+float k_val1 = 20;
+char KineFtr = 'f';
+int zero = 0;
+
 void setup()
 {
   Serial.begin(115200);
@@ -6,14 +17,25 @@ void setup()
 
 void loop()
 {
-  float hdr = 8;
-  float msg = 14;
-  float spd = 19;
-  float ftr = 21;
-   
-  Serial.println(hdr);
-  Serial.println(msg);
-  Serial.println(spd);
-  Serial.println(ftr);
+  if (Serial.read = 'E')
+  {
+    Serial.println(EcgHdr);
+    Serial.println(BPM);
+    Serial.println(MbFlag);
+    Serial.println(LvpFlag);
+    Serial.println(LvpValue);
+    Serial.println(EcgFtr);    
+  }
+  
+  if (Serial.read = 'K')
+  {
+    Serial.println(KineHdr);
+    Serial.println(k_val1);
+    Serial.println(zero);
+    Serial.println(zero);
+    Serial.println(zero);
+    Serial.println(KineFtr); 
+  }
+  
 }
 
