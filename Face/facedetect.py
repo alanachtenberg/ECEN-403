8ad4d4d3d1e48ad27a144ea8db2d0621ad177da1
alanachtenberg <alanachtenberg@tamu.edu>
2015-11-25 02:24:59 -0600

    clean up and some tuning of detection parameters, getting 40-50 fps on the desktop

--- Face/facedetect.py
+++ Face/facedetect.py
@@ -6,36 +6,40 @@ import cv2.cv as cv
 from video import create_capture
 from common import clock, draw_str
 
-help_message = '''
-USAGE: facedetect.py [--cascade <cascade_fn>] [--nested-cascade <cascade_fn>] [<video_source>]
-'''
+FACE_CASCADE_FILE_NAME = "./cascades/haarcascade_frontalface_alt.xml"
+EYE_CASCADE_FILE_NAME = "./cascades/haarcascade_eye.xml"
+VIDEO_SOURCE = 0
 
-def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=3, minSize=(45, 45), flags = cv.CV_HAAR_SCALE_IMAGE)
+
+def detect_face(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=2, minSize=(55, 55),
+                                     flags=cv.CV_HAAR_SCALE_IMAGE)
     if len(rects) == 0:
         return []
-    rects[:,2:] += rects[:,:2]
+    rects[:, 2:] += rects[:, :2]
     return rects
+
+
+def detect_eyes(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3, minSize=(12, 12),
+                                     flags=cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:, 2:] += rects[:, :2]
+    return rects
+
 
 def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
+
 if __name__ == '__main__':
-    import sys, getopt
-    print help_message
 
-    args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
-    try: video_src = video_src[0]
-    except: video_src = 3
-    args = dict(args)
-    cascade_fn = args.get('--cascade', "./cascades/haarcascade_frontalface_alt.xml")
-    nested_fn  = args.get('--nested-cascade', "./cascades/haarcascade_eye.xml")
+    cascade = cv2.CascadeClassifier(FACE_CASCADE_FILE_NAME)
+    nested = cv2.CascadeClassifier(EYE_CASCADE_FILE_NAME)
 
-    cascade = cv2.CascadeClassifier(cascade_fn)
-    nested = cv2.CascadeClassifier(nested_fn)
-
-    cam = create_capture(video_src, fallback='synth:bg=../cpp/lena.jpg:noise=0.05')
+    cam = create_capture(VIDEO_SOURCE, fallback='synth:bg=../cpp/lena.jpg:noise=0.05')
 
     while True:
         ret, img = cam.read()
@@ -43,17 +47,17 @@ if __name__ == '__main__':
         gray = cv2.equalizeHist(gray)
 
         t = clock()
-        rects = detect(gray, cascade)
+        rects = detect_face(gray, cascade)
         vis = img.copy()
         draw_rects(vis, rects, (0, 255, 0))
         for x1, y1, x2, y2 in rects:
             roi = gray[y1:y2, x1:x2]
             vis_roi = vis[y1:y2, x1:x2]
-            subrects = detect(roi.copy(), nested)
+            subrects = detect_eyes(roi.copy(), nested)
             draw_rects(vis_roi, subrects, (255, 0, 0))
         dt = clock() - t
 
-        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt * 1000))
         cv2.imshow('facedetect', vis)
 
         if 0xFF & cv2.waitKey(5) == 27:
