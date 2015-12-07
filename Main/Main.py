a59f6089ee83a0620fa1d3ce7050d01f19c57397
alanachtenberg <alanachtenberg@tamu.edu>
2015-12-06 22:00:21 -0600

    moved define before main

--- Main/Main.py
+++ Main/Main.py
@@ -40,6 +40,39 @@ def face_detection_handler(face, eyes):
             gpio.writePinValue(output_pin_2, "0")
 
 
+def fill_dict(serialValues, UdooGate, serialValueDict):
+    while not serialValues.empty():
+        data = serialValues.get()
+        if data == 'E':
+            serialValueDict['TYPE'] = "ECG"
+            serialValueDict['EcgHdr'] = data
+            serialValueDict['HEART_RATE'] = serialValues.get()
+            serialValueDict['MISSED_BEAT'] = serialValues.get()
+            serialValueDict['LOW_VOLT_PEAK'] = serialValues.get()
+            serialValueDict['LOW_VOLT_VAL'] = serialValues.get()
+            serialValueDict['EcgFtr'] = serialValues.get()
+            print(serialValueDict)
+
+            break
+
+        elif data == 'k':
+            serialValueDict['TYPE'] = "VEHICLE"
+            serialValueDict['KineHdr'] = data
+            serialValueDict['COLLISION'] = serialValues.get()
+            serialValueDict['VELOCITY'] = serialValues.get()
+            serialValueDict['zero1'] = serialValues.get()
+            serialValueDict['zero2'] = serialValues.get()
+            serialValueDict['KineFtr'] = serialValues.get()
+            print(serialValueDict)
+            break
+
+        else:
+            serialValues.put(data)
+
+
+
+
+
 if __name__ == '__main__':
     logging.basicConfig(handlers=[logging.StreamHandler()], level=logging.DEBUG)
     gpio.init_gpio(output_pin_1,output_pin_2)
@@ -70,38 +103,8 @@ if __name__ == '__main__':
         serialValues.put(sData5)
 
         fill_dict(serialValues, UdooGate, serialValueDict)
-        logging.debug(serialValueDict)
-        btComm.send()
+        btComm.send(serialValueDict)
 
     print("main exit")
     sys.exit()
 
-
-def fill_dict(serialValues, UdooGate, serialValueDict):
-    while not serialValues.empty():
-        data = serialValues.get()
-        if data == 'E':
-            serialValueDict['TYPE'] = "ECG"
-            serialValueDict['EcgHdr'] = data
-            serialValueDict['HEART_RATE'] = serialValues.get()
-            serialValueDict['MISSED_BEAT'] = serialValues.get()
-            serialValueDict['LOW_VOLT_PEAK'] = serialValues.get()
-            serialValueDict['LOW_VOLT_VAL'] = serialValues.get()
-            serialValueDict['EcgFtr'] = serialValues.get()
-            print(serialValueDict)
-
-            break
-
-        elif data == 'k':
-            serialValueDict['TYPE'] = "VEHICLE"
-            serialValueDict['KineHdr'] = data
-            serialValueDict['COLLISION'] = serialValues.get()
-            serialValueDict['VELOCITY'] = serialValues.get()
-            serialValueDict['zero1'] = serialValues.get()
-            serialValueDict['zero2'] = serialValues.get()
-            serialValueDict['KineFtr'] = serialValues.get()
-            print(serialValueDict)
-            break
-
-        else:
-            serialValues.put(data)
